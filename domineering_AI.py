52487098d565a2b85f66c2a973d7b105b0617f0d
Maciej Zagórski <s23575@pjwstk.edu.pl>
2023-10-09 12:53:16 +0200

    Using sets and tuples instead of lists to speed up

--- domineering_AI.py
+++ domineering_AI.py
@@ -1,12 +1,13 @@
 from easyAI import TwoPlayerGame, AI_Player, Human_Player, Negamax
-# import time
+import time
+
 
 class Domineering(TwoPlayerGame):
 
     def __init__(self, players, board_size):
         self.board_size = board_size
         self.board_size_len = len(str(board_size))
-        self.board = [(i, j) for i in range(1, board_size + 1) for j in range(1, board_size + 1)]
+        self.board = {(i, j) for i in range(1, board_size + 1) for j in range(1, board_size + 1)}
 
         self.players = players
         self.current_player = 1
@@ -23,34 +24,31 @@ class Domineering(TwoPlayerGame):
         return self._generate_possible_moves(self.opponent)
 
     def _generate_possible_moves(self, ply):
-        generated_possible_moves = [
-            [field, (field[0] + ply.move_type[0], field[1] + ply.move_type[1])]
-            for field in self.board
-            if (not (0 in field)) and (
-                field[0] + ply.move_type[0], field[1] + ply.move_type[1]) in self.board
+        x, y = ply.move_type
+        return [
+            (field, (field[0] + x, field[1] + y))
+            for field in self.board if (field[0] + x, field[1] + y) in self.board
         ]
-        return generated_possible_moves
 
     def make_move(self, move):
-        for field in move:
-            index = self.board.index(field)
-            self.board[index] = (0, 0)
+        self.board.difference_update(move)
         self.player.score -= len(self.possible_moves_opponent())
 
     def unmake_move(self, move):
         self.player.score += len(self.possible_moves_opponent())
-        for field in move:
-            index = (field[0] - 1) * self.board_size + (field[1] - 1)
-            self.board[index] = field
+        self.board.update(move)
 
     def show(self):
-        for i, field in enumerate(self.board):
-            self._show_field(field)
-            if (i + 1) % self.board_size == 0:
-                print()
+        for i in range(1, self.board_size + 1):
+            for j in range(1, self.board_size + 1):
+                self._show_field((i, j))
+            print()
 
     def _show_field(self, field):
-        print(f"({field[0]:<{self.board_size_len}}.{field[1]:{self.board_size_len}})", end=" ")
+        if field in self.board:
+            print(f"({field[0]:<{self.board_size_len}}.{field[1]:{self.board_size_len}})", end=" ")
+        else:
+            print(f"({0:<{self.board_size_len}}.{0:{self.board_size_len}})", end=" ")
 
     def lose(self):
         return not self.possible_moves()
@@ -62,10 +60,13 @@ class Domineering(TwoPlayerGame):
         return self.lose()
 
 
-ai = Negamax(5)
-game = Domineering([AI_Player(ai), AI_Player(ai)], 6)
-# start = time.time()
+ai = Negamax(15)
+game = Domineering([AI_Player(ai), AI_Player(ai)], 5)
+
+start = time.time()
+
 game.play()
 print("player %d loses" % game.current_player)
-# end = time.time()
-# print(end - start)
+
+end = time.time()
+print(end - start)
