56af2bd00cb1dbfc19f81f8381fbd19efb1e7d07
Maciej Zagórski <s23575@pjwstk.edu.pl>
2023-10-25 19:58:50 +0200

    code optimization and simplification

--- 02_Fuzzy_Logic/Simulation/Animation.py
+++ 02_Fuzzy_Logic/Simulation/Animation.py
@@ -2,13 +2,14 @@ import matplotlib.animation as animation
 import RoadJunction as rj
 import TrafficLightControlSystem as tlcs
 
-LEGEND_PARAMETERS_INDEX = 2
 DECREASE_CARS_RATE = 5
-AIR_CHANGE_RATE = 500
+PARAMETERS_CHANGE_RATE = 250
+EMERGENCY_THRESHOLD = 0.8
+
 
 class Animator:
     def __init__(self, figure, function):
-        self.animation = animation.FuncAnimation(figure, function, interval=100, cache_frame_data=False)
+        self.animation = animation.FuncAnimation(figure, function, interval=75, cache_frame_data=False)
         self.paused = True
         figure.canvas.mpl_connect('button_press_event', self._toggle_pause)
 
@@ -40,9 +41,6 @@ class Animation:
         self.plot.x_plot[0].set_linewidth(self.parameters.cars_queuing_x)
         self.plot.y_plot[0].set_linewidth(self.parameters.cars_queuing_y)
 
-        [self.plot.update_legend_text(i, self.parameters.get_random_parameters()[i - LEGEND_PARAMETERS_INDEX]) for i in
-         range(LEGEND_PARAMETERS_INDEX, len(self.plot.legend.get_texts()))]
-
     def _update(self, i):
         if i == 0:
             self.__initialize_simulation()
@@ -51,13 +49,16 @@ class Animation:
             self.__update_marker_and_switch()
 
         if i % self.increase_cars_rate == 0:
-            self.__increase_cars_number()
+            self.__adjust_car_number(self.plot.x_plot[0], 1)
+            self.__adjust_car_number(self.plot.y_plot[0], 1)
 
         if i % DECREASE_CARS_RATE == 0:
-            self.__decrease_cars_number()
+            plot_to_decrease = self.plot.x_plot[0] if self.switch_x_y else self.plot.y_plot[0]
+            self.__adjust_car_number(plot_to_decrease, -1)
 
-        if i % AIR_CHANGE_RATE == 0:
+        if i % PARAMETERS_CHANGE_RATE == 0:
             self.__update_air_transparency()
+            self.__update_emergency()
 
         self.__update_legend(i)
 
@@ -65,32 +66,51 @@ class Animation:
         self.animator.animation.pause()
         self.plot.switch_lights(self.switch_x_y)
         self.marker = round(
-            self.tlcs.perform_simulation(self.parameters.time_of_day, self.parameters.cars_queuing_x,
-                                         self.parameters.air_transparency) * 10, 0)
+            self.tlcs.perform_simulation(self.parameters.time_of_day,
+                                         self.parameters.cars_queuing_x,
+                                         self.parameters.air_transparency,
+                                         self.parameters.emergency)
+            * 10, 0)
 
     def __update_legend(self, i):
-        self.plot.update_legend_text(0, i)
-        self.plot.update_legend_text(1, self.marker)
-        self.plot.update_legend_text(3, self.parameters.air_transparency)
-        self.plot.update_legend_text(4, self.plot.x_plot[0].get_linewidth())
-        self.plot.update_legend_text(5, self.plot.y_plot[0].get_linewidth())
+        legend_values = [i,
+                         self.marker,
+                         self.parameters.time_of_day,
+                         self.plot.x_plot[0].get_linewidth(),
+                         self.plot.y_plot[0].get_linewidth(),
+                         self.parameters.air_transparency,
+                         self.parameters.emergency]
+
+        for idx, value in enumerate(legend_values):
+            self.plot.update_legend_text(idx, value)
+
+        if self.parameters.emergency > EMERGENCY_THRESHOLD:
+            self.plot.legend.get_texts()[6].set_color('red')
+        else:
+            self.plot.legend.get_texts()[6].set_color('black')
 
     def __update_marker_and_switch(self):
         new_linewidth = self.plot.x_plot[0].get_linewidth() if self.switch_x_y else self.plot.y_plot[0].get_linewidth()
-        self.marker += round(self.tlcs.perform_simulation(self.parameters.time_of_day, new_linewidth,
-                                                          self.parameters.air_transparency) * 10, 0)
+
+        self.marker += round(self.tlcs.perform_simulation(self.parameters.time_of_day,
+                                                          new_linewidth,
+                                                          self.parameters.air_transparency,
+                                                          self.parameters.emergency)
+                             * 10, 0)
+
         self.switch_x_y = not self.switch_x_y
         self.plot.switch_lights(self.switch_x_y)
 
-    def __increase_cars_number(self):
-        self.plot.x_plot[0].set_linewidth((self.plot.x_plot[0].get_linewidth()) + 1)
-        self.plot.y_plot[0].set_linewidth((self.plot.y_plot[0].get_linewidth()) + 1)
+        if self.parameters.emergency > EMERGENCY_THRESHOLD:
+            self.parameters.change_emergency()
 
-    def __decrease_cars_number(self):
-        plot_do_decrease = self.plot.x_plot[0] if self.switch_x_y else self.plot.y_plot[0]
-        if plot_do_decrease.get_linewidth() > 0:
-            plot_do_decrease.set_linewidth(plot_do_decrease.get_linewidth() - 1)
+    def __adjust_car_number(self, plot, change):
+        new_width = plot.get_linewidth() + change
+        plot.set_linewidth(max(new_width, 0))
 
     def __update_air_transparency(self):
         self.parameters.change_air_transparency()
         self.plot.fig.patch.set_alpha((100 - self.parameters.air_transparency) / 100)
+
+    def __update_emergency(self):
+        self.parameters.change_emergency()
