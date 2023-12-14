96953ac5296dc0af9739927ee1864fcae9ab26f6
Maciej Zagórski <s23575@pjwstk.edu.pl>
2023-12-14 13:06:38 +0100

    Added second model to Fashion MNIST problem

--- 05_NeuralNetworks/3_tf_fashion_mnist/3_tf_fashion_mnist.py
+++ 05_NeuralNetworks/3_tf_fashion_mnist/3_tf_fashion_mnist.py
@@ -6,7 +6,8 @@ import tensorflow as tf
 
 # https://github.com/zalandoresearch/fashion-mnist
 SAMPLES_PATH = "3_fashion_mnist_samples/"
-MODEL_NAME = "3_fashion_mnist_model.keras"
+MODEL_NAME_1 = "3_fashion_mnist_model_1.keras"
+MODEL_NAME_2 = "3_fashion_mnist_model_2.keras"
 
 class_names = ['t-shirt/top', 'trouser', 'pullover', 'dress', 'coat', 'sandal', 'shirt', 'sneaker', 'bag', 'ankle boot']
 
@@ -34,36 +35,59 @@ generate_samples(samples_train, X_train, y_train)
 generate_samples(samples_test, X_test, y_test)
 
 try:
-    model = tf.keras.models.load_model(MODEL_NAME)
+    model_1 = tf.keras.models.load_model(MODEL_NAME_1)
 except:
-    model = None
-if not model:
-    d = len(X_train[0])
-    n_hidden = 100
-    l_rate = .001
+    model_1 = None
+if not model_1:
     n_epochs = 50
 
-    model = tf.keras.Sequential([
+    model_1 = tf.keras.Sequential([
         tf.keras.layers.Flatten(input_shape=(28, 28), name="X"),
         tf.keras.layers.Dense(128, activation="relu"),
-        tf.keras.layers.Dense(128, activation="hard_sigmoid"),
+        tf.keras.layers.Dense(128, activation="relu"),
+        tf.keras.layers.Dense(128, activation="relu"),
         tf.keras.layers.Dropout(0.2),
         tf.keras.layers.Dense(10, activation="softmax"),
     ])
 
-    model.compile(optimizer="adam", loss="sparse_categorical_crossentropy", metrics=["accuracy"])
-    model.summary()
-    model.fit(X_train, y_train, epochs=n_epochs)
-    model.save(MODEL_NAME)
+    model_1.compile(optimizer="adam", loss="sparse_categorical_crossentropy", metrics=["accuracy"])
+    model_1.summary()
+    model_1.fit(X_train, y_train, epochs=n_epochs)
+    model_1.save(MODEL_NAME_1)
 
-train_accuracy = model.evaluate(X_train, y_train)[1]
-test_accuracy = model.evaluate(X_test, y_test)[1]
+train_accuracy_model_1 = model_1.evaluate(X_train, y_train)[1]
+test_accuracy_model_1 = model_1.evaluate(X_test, y_test)[1]
 
-print(f"Train Classification Accuracy: {train_accuracy:.4f}")
-print(f"Test Classification Accuracy: {test_accuracy:.4f}")
+print(f"Train Classification Accuracy (Model 1: 3 Layers): {train_accuracy_model_1:.4f}")
+print(f"Test Classification Accuracy (Model 1: 3 Layers): {test_accuracy_model_1:.4f}")
+
+try:
+    model_2 = tf.keras.models.load_model(MODEL_NAME_2)
+except:
+    model_2 = None
+if not model_2:
+    n_epochs = 50
+
+    model_2 = tf.keras.Sequential([
+        tf.keras.layers.Flatten(input_shape=(28, 28), name="X"),
+        tf.keras.layers.Dense(384, activation="relu"),
+        tf.keras.layers.Dropout(0.2),
+        tf.keras.layers.Dense(10, activation="softmax"),
+    ])
+
+    model_2.compile(optimizer="adam", loss="sparse_categorical_crossentropy", metrics=["accuracy"])
+    model_2.summary()
+    model_2.fit(X_train, y_train, epochs=n_epochs)
+    model_2.save(MODEL_NAME_2)
+
+train_accuracy_model_2 = model_2.evaluate(X_train, y_train)[1]
+test_accuracy_model_2 = model_2.evaluate(X_test, y_test)[1]
+
+print(f"Train Classification Accuracy (Model 2: 1 Layer): {train_accuracy_model_2:.4f}")
+print(f"Test Classification Accuracy (Model 2: 1 Layer): {test_accuracy_model_2:.4f}")
 
 
-def generate_predictions(model, samples):
+def generate_predictions(models, samples):
     print("\n* * * Validation of the Neural Network on Samples * * *\n")
     for s in samples:
         image_path = SAMPLES_PATH + s
@@ -75,11 +99,12 @@ def generate_predictions(model, samples):
         image = image.reshape((28, 28))
         image = np.expand_dims(image, axis=0)
 
-        prediction = model.predict(image)
         print(f"\nImage: {s}")
-        print(f"Predicted label: {class_names[prediction[0].argmax()]}")
+        for i, m in enumerate(models):
+            prediction = m.predict(image)
+            print(f"Predicted label (Model {i + 1}): {class_names[prediction[0].argmax()]}")
         print(f"Actual label: {class_names[int(s[-5])]}\n")
 
 
 samples_to_predict = os.listdir(SAMPLES_PATH)
-generate_predictions(model, samples_to_predict)
+generate_predictions([model_1, model_2], samples_to_predict)
