554b32d96db2315755dc1a720fd0270a16a51556
Maciej Zagórski <s23575@pjwstk.edu.pl>
2024-01-20 11:40:51 +0100

    Create main.py

---
+++ 07_Gym_RML/main.py
@@ -0,0 +1,18 @@
+import gymnasium as gym
+
+env = gym.make("ALE/Pacman-v5", render_mode='human')
+# env = gym.make("MsPacman-v4", render_mode='rgb_array')
+observation, info = env.reset()
+
+best_score = 0
+
+for _ in range(1000):
+    action = env.action_space.sample()
+    observation, reward, terminated, truncated, info = env.step(action)
+
+    if terminated or truncated:
+        observation, info = env.reset()
+
+    print(reward)
+
+env.close()
