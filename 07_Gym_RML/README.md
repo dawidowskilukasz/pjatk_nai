72cbce49f5017c37006b6bdac614adb22385c10c
Maciej Zagórski <s23575@pjwstk.edu.pl>
2024-02-01 20:09:45 +0100

    Adding documentation and generated models

---
+++ 07_Gym_RML/README.md
@@ -0,0 +1,42 @@
+## Reinforcement Learning
+
+#### Installation:
+
+Assuming that you have pip installed, type this in a terminal:  
+* ```sudo pip install stable-baselines3``` (with regard to creating reinforcement learning [“RL”] models),  
+* ```sudo pip install gymnasium[accept-rom-license]``` (with regard to Atari environments, used to teach RL models) 
+
+#### Overview:
+
+A simple program using _Stable Baselines3_ – a set of reliable implementations of RL algorithms – to create AI models
+(agents) able to play in one of the classic Atari games, i.e. _Pacman_ in this case. You can find presentations of
+(videos with) the application of the developed agents in the game below. The agents were created using the following
+algorithms and using CNN policies (https://stable-baselines.readthedocs.io/en/master/modules/policies.html):
+* A2C – synchronous, deterministic variant of Asynchronous Advantage Actor Critic (A3C):
+https://stable-baselines3.readthedocs.io/en/master/modules/a2c.html
+* DQN – Deep Q Network: https://stable-baselines3.readthedocs.io/en/master/modules/dqn.html
+* PPO – Proximal Policy Optimization: https://stable-baselines3.readthedocs.io/en/master/modules/ppo.html  
+
+An attempt was also made to create models using the _TensorFlow_ library and _Keras_ API, but this ended in failure
+(because of the problems resulting from the libraries versions and their compatibility). Therefore, it was decided to
+use _Stable Baselines3_ instead.
+
+#### Authors:
+
+By Maciej Zagórski (s23575) and Łukasz Dawidowski (s22621), group 72c (10:15-11:45)
+
+#### Sources:
+
+https://stable-baselines3.readthedocs.io/ (Stable-Baselines3 documentation)
+https://gymnasium.farama.org/ (Gymnasium documentation)
+
+#### Screens:
+
+Learning process:  
+![Learning process](LearningProcess.png)  
+A2C agent:  
+[![A2C agent screenshot](https://img.youtube.com/vi/TPRT-t14pi0/hqdefault.jpg)](https://youtu.be/TPRT-t14pi0)  
+DQN agent:
+[![DQN agent screenshot](https://img.youtube.com/vi/jLubnVmyucY/hqdefault.jpg)](https://youtu.be/jLubnVmyucY)
+PPO agent:
+[![PPO agent screenshot](https://img.youtube.com/vi/jLubnVmyucY/hqdefault.jpg)](https://youtu.be/jLubnVmyucY)
--- 07_Gym_RML/rl_pacman_sb3.py
+++ 07_Gym_RML/rl_pacman_sb3.py
@@ -1,3 +1,26 @@
+"""
+Reinforcement Learning
+
+#### Installation:
+
+Assuming that you have pip installed, type this in a terminal:
+– sudo pip install stable-baselines3 (with regard to creating reinforcement learning [“RL”] models),
+– sudo pip install gymnasium[accept-rom-license] (with regard to Atari environments, used to teach RL models)
+
+Overview:
+A simple program using Stable Baselines3 – a set of reliable implementations of RL algorithms – to create AI models
+(agents) able to play in one of the classic Atari games, i.e. Pacman in this case. You can find presentations of
+(videos with) the application of the developed agents in the game below. The agents were created using the following
+algorithms and using CNN policies (https://stable-baselines.readthedocs.io/en/master/modules/policies.html):
+– A2C – synchronous, deterministic variant of Asynchronous Advantage Actor Critic (A3C):
+https://stable-baselines3.readthedocs.io/en/master/modules/a2c.html
+– DQN – Deep Q Network: https://stable-baselines3.readthedocs.io/en/master/modules/dqn.html
+– PPO – Proximal Policy Optimization: https://stable-baselines3.readthedocs.io/en/master/modules/ppo.html
+
+Authors:
+By Maciej Zagórski (s23575) and Łukasz Dawidowski (s22621), group 72c (10:15-11:45)
+"""
+
 import os.path
 import gymnasium as gym
 from stable_baselines3 import PPO, DQN, A2C
@@ -7,31 +30,47 @@ from stable_baselines3.common.env_util import make_atari_env
 from stable_baselines3.common.vec_env import VecFrameStack
 
 GAME = "ALE/Pacman-v5"
-MODEL_TYPE = "DQN"  # Chose model type: PPO, DQN or A2C
-TIMESTEPS = int(100_000)  # Adjust model learning time
+MODEL_TYPE = "A2C"  # Chose model type: PPO, DQN or A2C
+TIMESTEPS = int(500_000)  # Adjust model learning time
 MODEL_NAME = MODEL_TYPE + "_pacman_" + str(TIMESTEPS)
 
 
 def check_file(filename):
+    """
+    Check if a file with the given filename exists in the current directory.
+    """
     return os.path.isfile("./" + filename + ".zip")
 
 
 def generate_model_dqn(env):
-    # https://stable-baselines3.readthedocs.io/en/master/modules/dqn.html#parameters
+    """
+    Generate a Deep Q Network (DQN) model using Stable Baselines3 for the given environment.
+    Algorithm parameters: https://stable-baselines3.readthedocs.io/en/master/modules/dqn.html#parameters
+    """
     return DQN("CnnPolicy", env, buffer_size=int(3e4), verbose=1)
 
 
 def generate_model_ppo(env):
-    # https://stable-baselines3.readthedocs.io/en/master/modules/ppo.html#parameters
+    """
+    Generate a Proximal Policy Optimization (PPO) model using Stable Baselines3 for the given environment.
+    Algorithm parameters: https://stable-baselines3.readthedocs.io/en/master/modules/ppo.html#parameters
+    """
     return PPO("CnnPolicy", env, verbose=1)
 
 
 def generate_model_a2c(env):
-    # https://stable-baselines3.readthedocs.io/en/master/modules/a2c.html#parameters
+    """
+    Generate an Asynchronous Advantage Actor Critic (A2C) model using Stable Baselines3 for the given environment.
+    Algorithm parameters: https://stable-baselines3.readthedocs.io/en/master/modules/a2c.html#parameters
+    """
     return A2C("CnnPolicy", env, verbose=1)
 
 
 def generate_model(game, model_type, timesteps):
+    """
+    Generate and save a reinforcement learning model based on the specified game, model type, and learning time.
+    """
+
     # env = gym.make(game, render_mode="rgb_array")
     # env = AtariWrapper(env)
 
@@ -49,6 +88,9 @@ def generate_model(game, model_type, timesteps):
 
 
 def load_model(game, model_name, model_type):
+    """
+    Load a pre-trained reinforcement learning model.
+    """
     env = gym.make(game, render_mode="human")
     env = AtariWrapper(env)
 
@@ -62,6 +104,9 @@ def load_model(game, model_name, model_type):
 
 
 def evaluate_model(model):
+    """
+    Evaluate and present the performance of a reinforcement learning model.
+    """
     mean_reward, std_reward = evaluate_policy(model, model.get_env(), n_eval_episodes=1)
     print(mean_reward, std_reward)
 
@@ -75,10 +120,14 @@ def evaluate_model(model):
 
 
 def main():
+    """
+    The main function that executes the reinforcement learning pipeline.
+    """
     if not check_file(MODEL_NAME):
         generate_model(GAME, MODEL_TYPE, TIMESTEPS)
 
     evaluate_model(load_model(GAME, MODEL_NAME, MODEL_TYPE))
 
 
-main()
+if __name__ == "__main__":
+    main()
